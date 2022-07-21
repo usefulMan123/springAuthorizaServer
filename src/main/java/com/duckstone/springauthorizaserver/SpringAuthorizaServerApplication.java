496fba94322a4bc7bb874ef00b8757b22b6844aa
dongks <2337441196@qq.com>
2022-07-21 11:54:51 +0800

    初始化创建项目

---
+++ src/main/java/com/duckstone/springauthorizaserver/SpringAuthorizaServerApplication.java
@@ -0,0 +1,13 @@
+package com.duckstone.springauthorizaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthorizaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthorizaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duckstone/springauthorizaserver/SpringAuthorizaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duckstone.springauthorizaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthorizaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
